ed0269425cfe2ad8787ab6f891599c1be6b669ef
John Vinh <john.michael.vinh2@gmail.com>
2022-11-25 10:59:09 -0500

    Create method for initializing every cell

--- src/ConwayView.java
+++ src/ConwayView.java
@@ -1,13 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ConwayView extends JFrame {
     JButton[][] cells;
+    private int dim;
 
     public ConwayView(int dim) {
         super("Conway's Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.cells = new JButton[dim][dim];
+        this.dim = dim;
         setSize(800, 600);
         setVisible(true);
+        initializeCells();
+    }
+
+    private void initializeCells() {
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                cells[i][j] = new JButton("");
+                cells[i][j].setBackground(Color.WHITE);
+            }
+        }
     }
 }
