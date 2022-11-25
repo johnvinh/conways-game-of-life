e8b3e4cac28ac4633cda0e6f4b0ef75f3b2eaf38
John Vinh <john.michael.vinh2@gmail.com>
2022-11-25 11:12:07 -0500

    Create buttons and add them to the frame

--- src/ConwayView.java
+++ src/ConwayView.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class ConwayView extends JFrame {
     JButton[][] cells;
     private int dim;
+    private JPanel panel;
 
     public ConwayView(int dim) {
         super("Conway's Game of Life");
@@ -11,10 +12,14 @@ public class ConwayView extends JFrame {
         this.cells = new JButton[dim][dim];
         this.dim = dim;
         setSize(800, 600);
-        setVisible(true);
-        initializeCells();
+
+        panel = new JPanel();
         GridLayout layout = new GridLayout(dim, dim);
-        setLayout(layout);
+        panel.setLayout(layout);
+        panel.setVisible(true);
+        initializeCells();
+        add(panel);
+        setVisible(true);
     }
 
     private void initializeCells() {
@@ -22,6 +27,8 @@ public class ConwayView extends JFrame {
             for (int j = 0; j < dim; j++) {
                 cells[i][j] = new JButton("");
                 cells[i][j].setBackground(Color.WHITE);
+                cells[i][j].setVisible(true);
+                panel.add(cells[i][j]);
             }
         }
     }
