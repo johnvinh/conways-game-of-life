c4d7f6078bcf41d4335773e566b2bf6e08f75084
John Vinh <john.michael.vinh2@gmail.com>
2022-11-25 11:20:01 -0500

    Create members for accessing model and view

--- src/ConwayController.java
+++ src/ConwayController.java
@@ -1,2 +1,9 @@
 public class ConwayController {
+    ConwayModel model;
+    ConwayView view;
+
+    public ConwayController(ConwayModel model, ConwayView view) {
+        this.model = model;
+        this.view = view;
+    }
 }
