88fae3b6e0ab62104be3102aa12379d30fcc56eb
John Vinh <john.michael.vinh2@gmail.com>
2022-11-26 16:33:33 -0500

    Add model javadocs

--- src/ConwayModel.java
+++ src/ConwayModel.java
@@ -1,14 +1,34 @@
+/**
+ * @author John
+ * @version 1
+ * The MVC architecture model for Conway's Game of Life.
+ */
 public class ConwayModel {
+    /**
+     * Represents the square dimensions of the simulation
+     */
     private int dim;
 
+    /**
+     * Class constructor.
+     * @param dim   the square dimensions of the simulation
+     */
     public ConwayModel(int dim) {
         this.dim = dim;
     }
 
+    /**
+     * Gets the square dimensions of the simulation.
+     * @return  a number representing the square dimensions
+     */
     public int getDim() {
         return dim;
     }
 
+    /**
+     * Sets the square dimensions of the simulation.
+     * @param dim   a number representing the new square dimensions
+     */
     public void setDim(int dim) {
         this.dim = dim;
     }
